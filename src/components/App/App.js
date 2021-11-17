c9e1645ab6c167dd721cadb0072280e9aa027314
Steven Quintana <steve.e.quint@gmail.com>
2021-11-17 15:58:28 -0800

    Place the Ball component inside the canvas

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
+import { Canvas } from '@react-three/fiber'
 import Ball from '../Ball'
 
 const App = () => {
   return(
     <>
       <h1>The Decision Maker</h1>
-      <Ball />
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Ball position={[0, -1, 2]} />
+      </Canvas>
     </>
   )
 }
