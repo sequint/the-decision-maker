131dc27e90e9a54a7e9aeba3d726bcb5bd59a613
Steven Quintana <steve.e.quint@gmail.com>
2021-11-17 14:34:39 -0800

    Create an empty App component

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const App = () => {
+  return(
+    <>
+      <h1>The Decision Maker</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+// Components index file that exports all components as default.
+
+export { default } from './App/App'
