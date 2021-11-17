bb2b597648df48de7c1a14e47c34306263fa5ae7
Steven Quintana <steve.e.quint@gmail.com>
2021-11-17 15:58:12 -0800

    Create a 3D vertical roatating box

--- src/components/Ball/Ball.js
+++ src/components/Ball/Ball.js
@@ -1,9 +1,26 @@
+import { useState, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 
-const Ball = () => {
+const Ball = props => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => (mesh.current.rotation.x += 0.01))
+
   return(
-    <>
-      <h2>This is a ball</h2>
-    </>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 2, 3]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
   )
 }
 
