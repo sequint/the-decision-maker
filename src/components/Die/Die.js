4ea644f88199f9323a840cde5e185e81b47ab383
Steven Quintana <steve.e.quint@gmail.com>
2021-11-17 16:50:03 -0800

    Create function to change Die size dependant on click event states

--- src/components/Die/Die.js
+++ src/components/Die/Die.js
@@ -7,19 +7,35 @@ const Die = props => {
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
+  const [hold, setHold] = useState(false)
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => (mesh.current.rotation.y += 0.008))
+
+  // Control Die size dependant on click events
+  const changeSize = () => {
+    if (hold) {
+      return .5
+    }
+    else if (active) {
+      return 1.5
+    }
+    else {
+      return 1
+    }
+  }
 
   return(
     <mesh
       {...props}
       ref={mesh}
-      scale={active ? 1.5 : 1}
+      scale={changeSize()}
       onClick={(event) => setActive(!active)}
+      onPointerDown={(event) => setHold(true)}
+      onPointerUp={(event) => setHold(false)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color={hovered ? 'purple' : 'grey'} />
+      <meshStandardMaterial color={hovered ? 'lightgrey' : 'grey'} />
     </mesh>
   )
 }
