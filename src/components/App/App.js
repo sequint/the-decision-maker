8785ebd88f544c4cd4b8c55fd88f1e416ceef8b3
Steven Quintana <steve.e.quint@gmail.com>
2021-11-17 16:22:22 -0800

    Update ball size modifying height in css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
 import * as React from 'react'
 import { Canvas } from '@react-three/fiber'
 import Ball from '../Ball'
+import './App.css'
 
 const App = () => {
   return(
     <>
       <h1>The Decision Maker</h1>
-      <Canvas>
+      <Canvas id="canvas">
         <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Ball position={[0, -1, 2]} />
+        <pointLight position={[0, 0, 10]} />
+        <Ball position={[0, 0, 0]} />
       </Canvas>
     </>
   )
